b01bffdb7b0dcae55dabc3c8d11efe991e810128
Nick Li <shao-peng.li@houzz.com>
2022-08-10 01:44:40 +0800

    Add OSM example

--- pet-limitless-web/src/main.ts
+++ pet-limitless-web/src/main.ts
@@ -5,6 +5,9 @@ import BootstrapVue3 from "bootstrap-vue-3";
 import App from "./App.vue";
 import router from "./router";
 
+import OpenLayersMap from "vue3-openlayers";
+import "vue3-openlayers/dist/vue3-openlayers.css";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
@@ -15,5 +18,6 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(BootstrapVue3);
+app.use(OpenLayersMap);
 
 app.mount("#app");
